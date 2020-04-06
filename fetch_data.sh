9e19bddeadb853a702d924539f8f6e5c160dbe48
Jason Alderman <jason.alderman@sunfolding.com>
2020-04-06 09:19:13 -0700

    Added a script to fetch the latest NYT data.

---
+++ fetch_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -q https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv -O us-counties.csv
+wget -q https://github.com/nytimes/covid-19-data/raw/master/us-states.csv -O us-states.csv
