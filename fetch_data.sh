fb9e0c6fc27d777bec23daafecf695b8161bbc46
Jason Alderman <jason.alderman@sunfolding.com>
2020-04-08 08:54:20 -0700

    Updated data

--- fetch_data.sh
+++ fetch_data.sh
@@ -2,3 +2,4 @@
 
 wget -q https://github.com/nytimes/covid-19-data/raw/master/us-counties.csv -O us-counties.csv
 wget -q https://github.com/nytimes/covid-19-data/raw/master/us-states.csv -O us-states.csv
+wget -q https://github.com/datadesk/california-coronavirus-data/raw/master/latimes-place-totals.csv -O latimes-place-totals.csv
